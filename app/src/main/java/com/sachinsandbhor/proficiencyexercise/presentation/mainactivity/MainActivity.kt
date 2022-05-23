92f5183dc53646737209a3abc0838f999fd10c2e
sachinsandbhor <sachinsandbhor@gmail.com>
2022-05-23 16:25:35 +0530

    initial commit of project

---
+++ app/src/main/java/com/sachinsandbhor/proficiencyexercise/presentation/mainactivity/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.sachinsandbhor.proficiencyexercise.presentation.mainactivity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.sachinsandbhor.proficiencyexercise.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
